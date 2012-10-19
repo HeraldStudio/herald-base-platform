4d68405273b9a323c38afddc62dab9f48bac2d0e
Wenhao Ji <predator.ray@gmail.com>
2012-10-19 19:20:07 +0800

    implemented the session app layer

---
+++ herald-session/herald-session-app/src/main/java/cn/edu/seu/herald/session/app/SessionApplication.java
@@ -0,0 +1,40 @@
+/*
+ * Copyright 2012 Herald, Southeast University.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package cn.edu.seu.herald.session.app;
+
+import org.restlet.Application;
+import org.restlet.Restlet;
+import org.restlet.routing.Router;
+
+/**
+ *
+ * @author rAy <predator.ray@gmail.com>
+ */
+public class SessionApplication extends Application {
+    
+    private Router sessionRouter;
+    
+    public SessionApplication() {
+        sessionRouter = new SessionRouter(getContext());
+    }
+    
+    @Override
+    public synchronized Restlet createInboundRoot() {
+        return sessionRouter;
+    }
+
+}
