37506ef2b82ae20f271122b804d539e0bc04b643
Wenhao Ji <predator.ray@gmail.com>
2012-10-28 22:48:17 +0800

    added mock student user account service implementation, removed spring dependency

--- herald-sso/herald-sso-core/src/main/java/cn/edu/seu/herald/sso/core/impl/StudentUserAccountServiceImpl.java
+++ herald-sso/herald-sso-core/src/main/java/cn/edu/seu/herald/sso/core/impl/StudentUserAccountServiceImpl.java
@@ -18,6 +18,8 @@ package cn.edu.seu.herald.sso.core.impl;
 
 import cn.edu.seu.herald.sso.core.StudentUserAccountService;
 import cn.edu.seu.herald.sso.domain.SingleSignOnContext;
+import cn.edu.seu.herald.sso.domain.StudentUser;
+import java.util.Enumeration;
 
 /**
  *
@@ -27,8 +29,37 @@ public class StudentUserAccountServiceImpl
         implements StudentUserAccountService {
 
     @Override
-    public SingleSignOnContext authenticate(String cardNumber, String password) {
-        throw new UnsupportedOperationException("Not supported yet.");
+    public SingleSignOnContext authenticate(String cardNumber,
+            String password) {
+        // a mock implementation
+        if ("213100000".equals(cardNumber) && "12345678".equals(password)) {
+            return new SingleSignOnContext() {
+                public StudentUser getLogOnStudentUser() {
+                    StudentUser sUsr = new StudentUser();
+                    sUsr.setCardNumber(213100000);
+                    sUsr.setFullName("Alice");
+                    sUsr.setStudentId("71110300");
+                    return sUsr;
+                }
+
+                public Object getAttribute(String name) {
+                    return null;
+                }
+
+                public Enumeration<String> getAttributeNames() {
+                    return new Enumeration<String>() {
+                        public boolean hasMoreElements() {
+                            return false;
+                        }
+
+                        public String nextElement() {
+                            return null;
+                        }
+                    };
+                }
+            };
+        }
+        return null;
     }
 
 }
